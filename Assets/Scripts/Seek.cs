3bde105a4df6550bad289004ac02e935ab01b7dc
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-21 15:04:17 -0500

    Added lab 2 base code

--- Assets/Scripts/Seek.cs
+++ Assets/Scripts/Seek.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
+// Apply this algorithm to the Steering.Seek method
 public class Seek : MonoBehaviour
 {
     public GameObject target;
-    float speed = 10.0f;
+    float moveSpeed = 10.0f;
     Rigidbody2D rb;
 
     Vector2 CurveSeek()
     {
         Vector2 currentVelocity = rb.linearVelocity;
-        Vector2 desiredVelocity = (target.transform.position - transform.position).normalized * speed;
+        Vector2 desiredVelocity = (target.transform.position - transform.position).normalized * moveSpeed;
         Vector2 seekForce = desiredVelocity - currentVelocity;
         return seekForce;
     }
@@ -21,27 +22,28 @@ public class Seek : MonoBehaviour
 
     void Update()
     {
-        rb.angularVelocity = 100.0f;
-        Vector3 direction = Quaternion.Euler(0.0f, 0.0f, rb.rotation) * Vector3.right;
-        Debug.DrawLine(transform.position, transform.position + direction * 5.0f);
-
         Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouse.z = 0.0f;
         Vector3 mouseDirection = (mouse - transform.position).normalized;
+        float mouseAngle = Vector3.SignedAngle(Vector3.right, mouseDirection, Vector3.forward);
 
         // Rotate at 720 degrees per second
-        float angularVelocity = 720.0f * Time.deltaTime;
+        float turnSpeed = 720.0f * Time.deltaTime;
 
-        // Angle from "zero" (aka Vector3.right which is the horizontal) to our mouse (+z is the axis of rotation since we're in 2d, hence Vector3.forward)
-        float angle = Vector3.SignedAngle(Vector3.right, mouseDirection, Vector3.forward);
+        // Apply rotation from current (rb rotation) to desired (mouse angle) at a maximum rate of angular velocity (turn speed)
+        rb.MoveRotation(Mathf.MoveTowardsAngle(rb.rotation, mouseAngle, turnSpeed));
 
-        // Apply rotation from current to desired at a maximum rate of angular velocity
-        rb.MoveRotation(Mathf.MoveTowardsAngle(rb.rotation, angle, angularVelocity));
-        
+        // Draw a line in the direction the seeker (rb) is facing, as a result of rb.MoveRotation from rb.rotation to the mouse cursor
+        Vector3 direction = Quaternion.Euler(0.0f, 0.0f, rb.rotation) * Vector3.right;
+        Debug.DrawLine(transform.position, transform.position + direction * 5.0f);
+
         // Instantaneously snaps the rotation to the input angle
         //rb.MoveRotation(angle);
 
-        Vector2 seekForce = CurveSeek();
+        //Vector2 seekForce = CurveSeek();
+        //rb.AddForce(seekForce);
+
+        Vector2 seekForce = Steering.Seek(rb, target.transform.position, moveSpeed, turnSpeed);
         rb.AddForce(seekForce);
     }
 }
