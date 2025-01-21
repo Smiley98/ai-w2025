e8e835bee3c59b06eb7dfb878f8a8bb1dabc42b0
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-21 14:47:56 -0500

    Added angular velocity example

--- Assets/Scripts/Seek.cs
+++ Assets/Scripts/Seek.cs
@@ -3,20 +3,9 @@ using UnityEngine;
 public class Seek : MonoBehaviour
 {
     public GameObject target;
-    float speed = 2.0f;
+    float speed = 10.0f;
     Rigidbody2D rb;
 
-    // Optional task 1: Make a separate GameObject that uses LineSeek to illustrate the difference between line vs curve seek
-    // Outputs the *position change* to move FROM seeker TO target in a line
-    Vector3 LineSeek()
-    {
-        // FROM seeker TO target
-        Vector2 direction = (target.transform.position - transform.position).normalized;
-        Vector2 change = direction * speed * Time.deltaTime;
-        return change;
-    }
-
-    // Optional task 2: Add a function called CurveFlee that flees the target instead of seeking the target
     Vector2 CurveSeek()
     {
         Vector2 currentVelocity = rb.linearVelocity;
@@ -25,8 +14,6 @@ public class Seek : MonoBehaviour
         return seekForce;
     }
 
-    // Optional task 3: Write code to rotate the seeker in the direction of its velocity (2% in lab 2, due on the 26th)
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -34,8 +21,25 @@ public class Seek : MonoBehaviour
 
     void Update()
     {
-        // Line seek is a velocity, so apply it directly to our seeker's position!
-        //transform.position += LineSeek();
+        rb.angularVelocity = 100.0f;
+        Vector3 direction = Quaternion.Euler(0.0f, 0.0f, rb.rotation) * Vector3.right;
+        Debug.DrawLine(transform.position, transform.position + direction * 5.0f);
+
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0.0f;
+        Vector3 mouseDirection = (mouse - transform.position).normalized;
+
+        // Rotate at 720 degrees per second
+        float angularVelocity = 720.0f * Time.deltaTime;
+
+        // Angle from "zero" (aka Vector3.right which is the horizontal) to our mouse (+z is the axis of rotation since we're in 2d, hence Vector3.forward)
+        float angle = Vector3.SignedAngle(Vector3.right, mouseDirection, Vector3.forward);
+
+        // Apply rotation from current to desired at a maximum rate of angular velocity
+        rb.MoveRotation(Mathf.MoveTowardsAngle(rb.rotation, angle, angularVelocity));
+        
+        // Instantaneously snaps the rotation to the input angle
+        //rb.MoveRotation(angle);
 
         Vector2 seekForce = CurveSeek();
         rb.AddForce(seekForce);
