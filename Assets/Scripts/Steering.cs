4e751aa27bd568959805aed73fa0b3d1144864fc
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-28 15:00:07 -0500

    Finished obstacle avoidance and added homework tasks

--- Assets/Scripts/Steering.cs
+++ Assets/Scripts/Steering.cs
@@ -10,4 +10,9 @@ public static class Steering
         // (Instead of rotating towards the mouse cursor, you need to rotate it towards its velocity direction)
         return Vector2.zero;
     }
+
+    // Lab Exercise 3:
+    // Task 1: Make a function that calculates avoidance force similar to the above Seek function
+    // It should be a public static method so avoidance can be applied to any object with a Rigidbody2D - 1%
+    // Task 2: Add and test 2 more avoidance rays, resulting in a total of 4 rays - 2%
 }
