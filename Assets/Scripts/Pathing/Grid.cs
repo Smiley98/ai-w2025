07d336fecc6b0f1a1bc7f2bd00dd543240546922
Smiley98 <smileysofficialcinema@gmail.com>
2025-02-04 14:38:33 -0500

    Added grid and single row

---
+++ Assets/Scripts/Pathing/Grid.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    GameObject tilePrefab;
+
+    const int rows = 10;
+    const int cols = 20;
+
+    int[,] tiles =
+    {   //0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 <-- Columns
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, // 0
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 1
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 2
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 3
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 4
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 5
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 6
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 7
+        { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // 8
+        { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }  // 9
+    };                                                             // Rows ^
+
+    List<GameObject> tileObjects = new List<GameObject>();
+
+    void Start()
+    {
+        float x = 0.5f;
+        float y = 9.5f;
+        for (int i = 0; i < cols; i++)
+        {
+            GameObject tile = Instantiate(tilePrefab);
+            tile.transform.position = new Vector3(x, y);
+            tileObjects.Add(tile);
+            x += 1.0f;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
