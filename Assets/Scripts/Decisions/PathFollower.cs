fdb18d940e5c7e394f81e962eafd1f1b6b6f1a83
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-04 14:52:14 -0500

    Switched from interpolation to velocity

--- Assets/Scripts/Decisions/PathFollower.cs
+++ Assets/Scripts/Decisions/PathFollower.cs
@@ -2,27 +2,31 @@ using UnityEngine;
 
 public class PathFollower : MonoBehaviour
 {
+    // Logic variables
     [SerializeField]
     GameObject[] waypoints;
+    int curr = -1;
+    int next = 0;
 
-    float time = 0.0f;
-    int curr = 0;
-    int next = 1;
+    // Physics variables
+    Rigidbody2D rb;
+    float speed = 10.0f;
+    Vector2 direction = Vector2.zero;
 
-    void Update()
+    void Start()
     {
-        float dt = Time.deltaTime;
-        time += dt;
-        if (time > 1.0)
-        {
-            time = 0.0f;
-            curr++;
-            next++;
-            curr = curr % waypoints.Length;
-            next = next % waypoints.Length;
-        }
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = waypoints[0].transform.position;
+    }
 
-        Vector3 position = Vector3.Lerp(waypoints[curr].transform.position, waypoints[next].transform.position, time);
-        transform.position = position;
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        curr++;
+        next++;
+        curr = curr % waypoints.Length;
+        next = next % waypoints.Length;
+        transform.position = waypoints[curr].transform.position;
+        direction = (waypoints[next].transform.position - waypoints[curr].transform.position).normalized;
+        rb.linearVelocity = direction * speed;
     }
 }
