b2f6754e1451bf313479c256afc36b9c0930a282
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-18 15:45:11 -0400

    Finalized linear vs curved path following

--- Assets/Scripts/Decisions/PathFollower.cs
+++ Assets/Scripts/Decisions/PathFollower.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PathFollower : MonoBehaviour
 {
+    bool linear = true;
+
     // Logic variables
     [SerializeField]
     GameObject[] waypoints;
-    int curr = 0;
-    int next = 1;
+    int curr = -1;
+    int next = 0;
 
     // Physics variables
     Rigidbody2D rb;
@@ -25,20 +26,37 @@ public class PathFollower : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        if (linear)
+            OnLinearEnter();
     }
 
     void Update()
     {
+        // Toggle linear vs curved path following
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            linear = !linear;
+            if (linear)
+                OnLinearEnter();
+        }
+
+        // Only run if curved
+        if (linear)
+            return;
+
+        // Calculate seek target
         Vector2 A = waypoints[curr].transform.position;
         Vector2 B = waypoints[next].transform.position;
-        Vector2 ahead = (Vector2)transform.position + (B - A).normalized * lookAhead;
-        Vector2 proj = Projection.ProjectPointLine(A, B, ahead);
-        rb.AddForce(Steering.Seek(rb, proj, 10.0f, 0.0f));
+        Vector2 projCurr = Projection.ProjectPointLine(A, B, transform.position);
+        Vector2 projNext = projCurr + (B - A).normalized * lookAhead;
+        rb.AddForce(Steering.Seek(rb, projNext, 10.0f, 0.0f));
 
-        // Check if our look-ahead exceeds line AB
-        float t = Projection.ScalarProjectPointLine(A, B, ahead);
+        // Visualize projections
+        projVisCurr.transform.position = projCurr;
+        projVisNext.transform.position = projNext;
 
-        // If so, seek next set of waypoints
+        // Update waypoints if our look-ahead projection exceeds line AB
+        float t = Projection.ScalarProjectPointLine(A, B, projNext);
         if (t > 1.0f)
         {
             ++curr;
@@ -48,14 +66,28 @@ public class PathFollower : MonoBehaviour
         }
     }
 
-    //void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    curr++;
-    //    next++;
-    //    curr = curr % waypoints.Length;
-    //    next = next % waypoints.Length;
-    //    transform.position = waypoints[curr].transform.position;
-    //    direction = (waypoints[next].transform.position - waypoints[curr].transform.position).normalized;
-    //    rb.linearVelocity = direction * speed;
-    //}
+    void OnLinearEnter()
+    {
+        curr = -1;
+        next = 0;
+        transform.position = waypoints[0].transform.position;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Only run if linear
+        if (!linear)
+            return;
+
+        curr++;
+        next++;
+        curr = curr % waypoints.Length;
+        next = next % waypoints.Length;
+        transform.position = waypoints[curr].transform.position;
+        direction = (waypoints[next].transform.position - waypoints[curr].transform.position).normalized;
+        rb.linearVelocity = direction * speed;
+
+        projVisCurr.transform.position = waypoints[curr].transform.position;
+        projVisNext.transform.position = waypoints[next].transform.position;
+    }
 }
