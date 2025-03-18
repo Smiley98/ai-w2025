9e2b3d08c3d2fc5692ec9c044f1d2a27e7fc7612
Smiley98 <smileysofficialcinema@gmail.com>
2025-03-18 15:19:47 -0400

    Curved line following attempt 1

--- Assets/Scripts/Decisions/PathFollower.cs
+++ Assets/Scripts/Decisions/PathFollower.cs
@@ -1,32 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PathFollower : MonoBehaviour
 {
     // Logic variables
     [SerializeField]
     GameObject[] waypoints;
-    int curr = -1;
-    int next = 0;
+    int curr = 0;
+    int next = 1;
 
     // Physics variables
     Rigidbody2D rb;
     float speed = 10.0f;
     Vector2 direction = Vector2.zero;
 
+    [SerializeField]
+    GameObject projVisCurr;
+
+    [SerializeField]
+    GameObject projVisNext;
+
+    float lookAhead = 2.0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        transform.position = waypoints[0].transform.position;
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void Update()
     {
-        curr++;
-        next++;
-        curr = curr % waypoints.Length;
-        next = next % waypoints.Length;
-        transform.position = waypoints[curr].transform.position;
-        direction = (waypoints[next].transform.position - waypoints[curr].transform.position).normalized;
-        rb.linearVelocity = direction * speed;
+        Vector2 A = waypoints[curr].transform.position;
+        Vector2 B = waypoints[next].transform.position;
+        Vector2 ahead = (Vector2)transform.position + (B - A).normalized * lookAhead;
+        Vector2 proj = Projection.ProjectPointLine(A, B, ahead);
+        rb.AddForce(Steering.Seek(rb, proj, 10.0f, 0.0f));
+
+        // Check if our look-ahead exceeds line AB
+        float t = Projection.ScalarProjectPointLine(A, B, ahead);
+
+        // If so, seek next set of waypoints
+        if (t > 1.0f)
+        {
+            ++curr;
+            ++next;
+            curr %= waypoints.Length;
+            next %= waypoints.Length;
+        }
     }
+
+    //void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    curr++;
+    //    next++;
+    //    curr = curr % waypoints.Length;
+    //    next = next % waypoints.Length;
+    //    transform.position = waypoints[curr].transform.position;
+    //    direction = (waypoints[next].transform.position - waypoints[curr].transform.position).normalized;
+    //    rb.linearVelocity = direction * speed;
+    //}
 }
